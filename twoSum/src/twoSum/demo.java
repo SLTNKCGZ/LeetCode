1304280a3e552cda7edbf7b4ec8c8f20631db524
Your Name <sultankocagoz.448@gmail.com>
2024-12-13 19:10:03 +0300

    commit

--- twoSum/src/twoSum/demo.java
+++ twoSum/src/twoSum/demo.java
@@ -6,7 +6,9 @@ public class demo {
 		int[] nums= {2,5,7,8};
 		int target=9;
 		int[] solutions=twoSum(nums,target);
-
+		for(int i=0;i<solutions.length;i++){
+			System.out.println(solutions[i]+" ");
+		}
 	}
 	
 	    public static int[] twoSum(int[] nums, int target) {
